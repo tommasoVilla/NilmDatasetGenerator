af2b8af7b0e492afc35258f96d19f2d13d8e84d8
gianlucafa <fabozzigianluca@gmail.com>
2020-11-06 19:04:57 +0100

    added washingmachine

--- multi_appliances.py
+++ multi_appliances.py
@@ -10,8 +10,8 @@ def dishwasher(seconds_tenths_in_a_day):
 
     # With a certain probability dishwasher is not used during this day
     dishwasher_usage_prob = float(config['usage_probabilities']['dishwasher_usage_prob'])
-    x = np.random.randint(1, 1 / (1 - dishwasher_usage_prob))
-    if x == 1:
+    x = np.random.uniform()
+    if x > dishwasher_usage_prob:
         return data
 
     # Reading dishwasher model from configuration file
@@ -40,6 +40,49 @@ def dishwasher(seconds_tenths_in_a_day):
     activation_instant = int(np.random.normal(mu, sigma))
 
     # Building dishwasher data
+    for i in range(len(series)):
+        data[activation_instant + i] = series[i]
+
+    return data
+
+
+def washingmachine(seconds_tenths_in_a_day):
+    config = configparser.ConfigParser()
+    config.read('resources/config.ini')
+    data = np.zeros(seconds_tenths_in_a_day, dtype=None)
+
+    # With a certain probability washingmachine is not used during this day
+    washingmachine_usage_prob = float(config['usage_probabilities']['washingmachine_usage_prob'])
+    x = np.random.uniform()
+    if x > washingmachine_usage_prob:
+        return data
+
+    # Reading washingmachine model from configuration file
+    washingmachine_model = config['models']['washingmachine_model']
+
+    # Reading the number of possible pattern for the specified model
+    washingmachine_patterns = int(config['patterns_for_models']['washingmachine' + str(washingmachine_model)])
+
+    # Select a random pattern between those available and building the corresponding series
+    x = np.random.randint(1, washingmachine_patterns + 1)
+    filename = 'data/washingmachine/washingmachine_house' + str(washingmachine_model) + "_" + str(x) + ".CSV"
+    series = pd.read_csv(filename, header=None, usecols=[1])[1]
+
+    # Altering series
+    washingmachine_factor = float(config['alterations']['washingmachine_factor'])
+    washingmachine_noise_factor = float(config['alterations']['washingmachine_noise_factor'])
+    for i in range(len(series)):
+        series[i] = series[i] * np.random.uniform(washingmachine_factor - washingmachine_noise_factor,
+                                                  washingmachine_factor + washingmachine_noise_factor)
+
+    # Choosing a start instant
+    washingmachine_start = int(config['usage_start']['washingmachine_start'])
+    washingmachine_end = int(config['usage_start']['washingmachine_end'])
+    mu = np.random.randint(washingmachine_start, washingmachine_end)
+    sigma = int(config['usage_start']['washingmachine_sigma'])
+    activation_instant = int(np.random.normal(mu, sigma))
+
+    # Building washingmachine data
     for i in range(len(series)):
         data[activation_instant + i] = series[i]
 
