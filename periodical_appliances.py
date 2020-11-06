ad632a2ea8a3014c9bb15735c2f151b65f6bc932
Tommaso Villa <tommaso.villa96@gmail.com>
2020-11-06 15:15:38 +0100

    added dishwasher

--- periodical_appliances.py
+++ periodical_appliances.py
@@ -5,21 +5,21 @@ import pandas as pd
 
 def fridge(seconds_tenths_in_a_day):
 
+    # Reading fridge model from configuration file
     config = configparser.ConfigParser()
     config.read('resources/config.ini')
     fridge_model = config['models']['fridge_model']
+
+    # Reading fridge model pattern from specified model
     filename = 'data/fridge/fridge_house' + str(fridge_model) + ".CSV"
+    series = pd.read_csv(filename, header=None, usecols=[1])[1]
 
-    # 6932 rows has to be skipped to concat pattern cleanly
-    series = pd.read_csv(filename, header=None, usecols=[1], skiprows=6932)[1]
-
+    # Replicating fridge pattern during all day
     pattern_duration = len(series)
     pattern_in_a_day = seconds_tenths_in_a_day // pattern_duration
-
     data = np.zeros(seconds_tenths_in_a_day, dtype=None)
-
-    for i in range(pattern_in_a_day - 1):
-        for j in range(pattern_duration - 1):
+    for i in range(pattern_in_a_day):
+        for j in range(pattern_duration):
             data[i * pattern_duration + j] = series[j]
 
     return data
